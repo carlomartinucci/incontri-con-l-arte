5afb505ed1008954dde1e8fa4d44a42dd509cbee
Carlo Martinucci <carlo.martinucci@gmail.com>
2020-05-01 12:08:48 +0200

    Updates

---
+++ precache-manifest.95a5fe266bc9bf9f7037bc7fc9534aef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cedce2f9a8c9095f1324398338cfd6e",
+    "url": "/incontri-con-l-arte/index.html"
+  },
+  {
+    "revision": "0a24ba5067b92d805fb7",
+    "url": "/incontri-con-l-arte/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0a24ba5067b92d805fb7",
+    "url": "/incontri-con-l-arte/static/js/2.d7a6366b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/incontri-con-l-arte/static/js/2.d7a6366b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "621b6169d72cfd98586e",
+    "url": "/incontri-con-l-arte/static/js/main.d9f234fe.chunk.js"
+  },
+  {
+    "revision": "f99664285c6eaa39fcbc",
+    "url": "/incontri-con-l-arte/static/js/runtime-main.24b76992.js"
+  }
+]);
